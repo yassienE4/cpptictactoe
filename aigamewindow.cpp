5b5d19d4923cddefaae2759ba06163a058eac460
yassienE4 <yassienebrahim@gmail.com>
2025-03-03 22:34:21 +0200

    buttons

--- aigamewindow.cpp
+++ aigamewindow.cpp
@@ -18,6 +18,20 @@ AiGameWindow::AiGameWindow(int diff, QWidget *parent) :
     turn = 0;
     mcount = 0;
 
+    int startX = 0;  // Starting X position
+    int startY = 0;  // Starting Y position
+    int spacing = 100;  // Space between buttons
+    for(int i =0; i < 3; i++)
+    {
+        for(int j = 0; j <3; j++)
+        {
+            buttonMatrix[i][j] = new QPushButton();
+            buttonMatrix[i][j]->move(startX + j * spacing, startY + i * spacing);
+            buttonMatrix[i][j]->setFixedSize(40, 40);
+            buttonMatrix[i][j]->setParent(this);
+        }
+    }
+
 }
 
 AiGameWindow::~AiGameWindow()
